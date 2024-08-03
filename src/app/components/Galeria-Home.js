199df30e6410cb2fe9a02e6b52d3d78fad97f252
Gris Cazolatti <59493366+Cristianjn4@users.noreply.github.com>
2024-08-03 14:43:40 -0300

    att

--- src/app/components/Galeria-Home.js
+++ src/app/components/Galeria-Home.js
@@ -46,25 +46,6 @@ export function GaleriaHome() {
 
                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 p-20 ">
 
-
-        <div className="    ">
-            <img className="h-auto max-w-full rounded-lg border-white" src="thiago.nigro.jpg" alt=""/>   
-            <p className="text-white text-lg flex align items-center justify-center m-auto"> 
-            <svg className="w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
-            </svg> 
-            thiago.nigro        
-            </p>
-        </div>
-        <div className="    ">
-            <img className="h-auto max-w-full rounded-lg border-white" src="velloso01.jpg" alt=""/>
-            <p className="text-white text-lg flex align items-center justify-center m-auto"> 
-            <svg className="w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
-            </svg> 
-            velloso01 
-            </p>
-        </div>
         <div className="    ">
             <img className="h-auto max-w-full rounded-lg border-white" src="craqueneto.jpg" alt=""/>
             <p className="text-white text-lg flex align items-center justify-center m-auto"> 
@@ -185,6 +166,7 @@ export function GaleriaHome() {
         
 
     </div>
+
     </div>
  
 
