328134ba83423de69f20249378f6d5f2e9e43ef5
Gris Cazolatti <59493366+Cristianjn4@users.noreply.github.com>
2024-08-16 15:38:48 -0300

    ooo

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+import { Alerts } from "../components/Alerts";
 import { Duvidas } from "../components/Duvidas";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -8,6 +9,7 @@ export default function (){
   return(
     <>
       <Header/>
+      <Alerts/>
       <Duvidas/>
       <Footer/>
     </>
