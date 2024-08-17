782d0e2168489e7fb3bdc69d0fc18e3603089739
Gris Cazolatti <59493366+Cristianjn4@users.noreply.github.com>
2024-08-17 12:33:05 -0300

    att1708

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,12 +4,14 @@ import { Alerts } from "../components/Alerts";
 import { Duvidas } from "../components/Duvidas";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
 export default function (){
   return(
     <>
       <Header/>
       <Alerts/>
+      <Login/>
       <Duvidas/>
       <Footer/>
     </>
