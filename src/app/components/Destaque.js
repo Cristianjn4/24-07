c5d83fe9ab0a98681b30ac82d4783d43cd66074a
Gris Cazolatti <59493366+Cristianjn4@users.noreply.github.com>
2024-08-21 17:20:52 -0300

    21-08

--- src/app/components/Destaque.js
+++ src/app/components/Destaque.js
@@ -3,16 +3,26 @@ export function Destaque(){
         <div className="">
              <div className="pt-32 md:p-12 lg:px-16 lg:py-24 align items-center text-center bg-gray-100 pb-10">
                 <div className="mx-2 block lg:flex ltr:sm:text-left rtl:sm:text-right">
-                <img src="logoR.png" className="m-auto w-44 mb-5 rounded-full" />
-                
-                <p className="px-8 font-medium text-lg text-black pt-4 ">
+                <img src="logoR.png" className="m-auto w-44 mb-14 rounded-full" />
+
+                <div className="bg-white w-3/4 p-6 rounded-2xl shadow-lg m-auto">
+                <p className="px-4 lg:px-28 font-medium text-lg text-black pt-4 ">
                 <h3 className="text-3xl px-8 font-bold text-black py-4">
                     Destaque Loja Perfeita Simmons
                 </h3> 
-                Conheça a Simmons Casa Orner e tenha melhor experiência na escolha do seu colchão ideal!<br/>
+                Conheça a Simmons Casa Orner e tenha melhor experiência na escolha do seu colchão ideal!
                 A Exclusiva que é "Destaque Loja Perfeita" entre as mais de 130 lojas no Brasil. <br/>
-                <a className="ml-1 font-bold underline hover:text-gray-700" href="https://www.instagram.com/stories/highlights/17856368678956115/" target="blank">Confira agora mesmo!</a>
                 </p>
+                <div className="mt-6 relative ">
+                <a
+                href="https://www.instagram.com/stories/highlights/17856368678956115/"
+                target="blank" className="inline-block rounded-full bg-black px-10 py-3 text-sm font-medium text-white transition hover:bg-white hover:text-black border hover:border-black hover:font-medium focus:outline-none">
+                Confira agora mesmo!
+                </a>
+                </div>
+                </div>
+                
+                
                 
                 </div>
             </div>
--- src/app/components/Promotion.js
+++ src/app/components/Promotion.js
@@ -1,9 +1,17 @@
 export default function Promotion() {
     return(
-        
+
+        <div>
 <a href="https://api.whatsapp.com/send/?phone=5511995379301&text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+a+campanha+Bota+Fora+Simmons.&type=phone_number&app_absent=0" class="hidden lg:block md:block">
     <img class="object-cover w-full" src="promobotafora.png" alt="Bota Fora Banner"/>
 </a>
+
+<a href="https://api.whatsapp.com/send/?phone=5511995379301&text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+a+campanha+Bota+Fora+Simmons.&type=phone_number&app_absent=0" class="block lg:hidden md:hidden">
+    <img class="object-cover w-full" src="promobotafora.png" alt="Bota Fora Banner"/>
+</a>
+        </div>
+        
+
 
     )
 }
